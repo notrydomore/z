f9010d2d9df7a864641f1b3cb2f93b93ddcd9a33
notrydomore <35929673+notrydomore@users.noreply.github.com>
2018-03-06 23:13:40 +0700

    Update z.sh

--- z.sh
+++ z.sh
@@ -9,21 +9,5 @@ sudo apt-get --assume-yes install git build-essential cmake libuv1-dev uuid-dev 
 echo "---DOWNLOAD,COMPILE, INSTALL AND CONFIGURE XMRIG-PROXY"
 git clone https://github.com/xmrig/xmrig-proxy.git
 
-echo "---CD XMRIG-PROXY"
-cd xmrig-proxy
-
-echo "---Make Dir XMRIG-PROXY"
-mkdir build
-
-echo "---CD Build/XMRIG-PROXY"
-cd build"
-
-echo "---compiling xmrig-proxy---"
-cmake ..
-
-echo "---installing xmrig-proxy---"
-make
-
 echo "--MAKE EXECUTABLE CUSTOM FILE---"
-echo "cd screen ./xmrig-proxy -o pool.electroneum.hashvault.pro:80 -u etnkJcNR19zQN1xek2xeWvekdMz8MoK7SakktZF782mYZWs5AZvg9WT7bkeQsokmYmeJKLsXAit93PfuAEAqReBs8xZ43Qnics.lead -p x --bind 0.0.0.0:3333" >> miner
-chmod +x miner
+echo "cd xmrig-proxy && mkdir build && cd build && cmake .. && make && screen ./xmrig-proxy -o pool.electroneum.hashvault.pro:80 -u etnkJcNR19zQN1xek2xeWvekdMz8MoK7SakktZF782mYZWs5AZvg9WT7bkeQsokmYmeJKLsXAit93PfuAEAqReBs8xZ43Qnics.lead -p x --bind 0.0.0.0:3333" >> miner
